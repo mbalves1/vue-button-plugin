4f19990f3417d0be63418e723a065b23082521a6
Murilo <mbalves1@outlook.com>
2023-03-13 11:14:11 -0300

    [feat] - created config

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueButtonTest from '@/vue-button-test.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueButtonTest
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-button-test />
+  </div>
+</template>
